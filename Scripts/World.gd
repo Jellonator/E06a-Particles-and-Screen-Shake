db4e58693477d82f83c2041e2899e5882385f036
Jocelyn Beedie <jellonator00@gmail.com>
2020-02-20 12:09:30 -0500

    Did more thing

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
-onready var Camera = $Camera
+onready var camera = $Camera
 
-func _ready():
-	pass
-
-func _physics_process(delta):
-	pass
+func _input(event):
+	if event.is_action_pressed("action_a"):
+		camera.add_trauma(0.25)
+	if event.is_action_pressed("action_s"):
+		camera.add_trauma(0.5)
+	if event.is_action_pressed("action_d"):
+		camera.add_trauma(0.75)
+	if event.is_action_pressed("action_w"):
+		camera.add_trauma(1)
